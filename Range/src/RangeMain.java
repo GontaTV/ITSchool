0d240aa66a0be59df28e1f50f02435a694f6267f
GontaTV <gontatv@ipgg.sbras.ru>
2019-10-10 12:14:16 +0700

    add task Range

---
+++ Range/src/RangeMain.java
@@ -0,0 +1,15 @@
+public class RangeMain {
+    public static void main(String[] args) {
+        Range range1 = new Range(2, 8);
+
+        System.out.println(range1.isInside(5));
+
+        Range range2 = new Range(3, 15);
+
+        range1.setTo(10);
+        System.out.println(range2.isInside(12));
+
+
+        System.out.println(range2.getLength());
+    }
+}
