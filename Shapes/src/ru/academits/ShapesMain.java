ff2d017f04fdd4b54055cd47a9e01bfec5b97eaf
GontaTV <gontatv@ipgg.sbras.ru>
2019-11-05 11:11:10 +0700

    поправил методы d Triangle

--- Shapes/src/ru/academits/ShapesMain.java
+++ Shapes/src/ru/academits/ShapesMain.java
@@ -1,26 +1,18 @@
 package ru.academits;
 
-import ru.academits.inteface.gonta.Shapes;
+import ru.academits.inteface.gonta.Shape;
 import ru.academits.shapes.gonta.Circle;
 import ru.academits.shapes.gonta.Rectangle;
 import ru.academits.shapes.gonta.Square;
 import ru.academits.shapes.gonta.Triangle;
+import ru.academits.sort_shapes.gonta.SortShapesArea;
+import ru.academits.sort_shapes.gonta.SortShapesPerimeter;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class ShapesMain {
-    static class SortShapesArea {
-        static Comparator<Shapes> AreaComparator = new Comparator<Shapes>() {
-            @Override
-            public int compare(Shapes s1, Shapes s2) {
-                return (int) (s2.getArea() - s1.getArea());
-            }
-        };
-    }
-
     public static void main(String[] args) {
-        Shapes square = new Square(1);
+        Shape square = new Square(1);
 
         System.out.println("----------КВАДРАТ----------");
         System.out.println("ширина квадрата = " + square.getWidth());
@@ -28,7 +20,7 @@ public class ShapesMain {
         System.out.println("площадь квадрата = " + square.getArea());
         System.out.println("периметр квадрата = " + square.getPerimeter());
 
-        Shapes circle = new Circle(5);
+        Shape circle = new Circle(5);
 
         System.out.println();
         System.out.println("----------ОКРУЖНОСТЬ----------");
@@ -37,18 +29,16 @@ public class ShapesMain {
         System.out.println("площадь окружности = " + circle.getArea());
         System.out.println("длина окружности = " + circle.getPerimeter());
 
-        Shapes triangle = new Triangle(5, 3, 5, 1, 6, 9);
-        Shapes triangle1 = new Triangle(3, 3, 1, 8, 2, 9);
+        Shape triangle = new Triangle(5, 3, 5, 1, 6, 9);
 
         System.out.println();
         System.out.println("----------ТРЕУГОЛЬНИК----------");
         System.out.println("ширина треугольника = " + triangle.getWidth());
         System.out.println("высота треугольника = " + triangle.getHeight());
         System.out.println("площадь треугольника = " + triangle.getArea());
-        System.out.println("площадь треугольника = " + triangle1.getArea());
         System.out.println("периметр треугольника = " + triangle.getPerimeter());
 
-        Shapes rectangle = new Rectangle(4, 8);
+        Shape rectangle = new Rectangle(4, 8);
 
         System.out.println();
         System.out.println("----------ПРЯМОУГОЛЬНИК----------");
@@ -57,7 +47,7 @@ public class ShapesMain {
         System.out.println("площадь прямоугольника = " + rectangle.getArea());
         System.out.println("периметр прямоугольника = " + rectangle.getPerimeter());
 
-        Shapes[] shapes = {
+        Shape[] shapes = {
                 new Rectangle(3, 5),
                 new Triangle(2, 2, 5, 4, 5, 6),
                 new Circle(3),
@@ -67,22 +57,23 @@ public class ShapesMain {
                 new Square(7),
                 new Circle(5)};
 
-        Arrays.sort(shapes, SortShapesArea.AreaComparator);
+        Arrays.sort(shapes, new SortShapesArea());
 
         System.out.println();
         System.out.println("----------ПЛОЩАДЬ ФИГУР----------");
 
-        for (Shapes e : shapes) {
+        for (Shape e : shapes) {
             System.out.println(e.getArea());
         }
 
         System.out.println();
         System.out.println("Фигура с максимальной площадью - " + shapes[0]);
         System.out.println();
-        System.out.println("Фигура со второй по величине площадью - " + shapes[1]);
+        Arrays.sort(shapes, new SortShapesPerimeter());
+        System.out.println("Фигура со вторым по величине периметром - " + shapes[1]);
 
-        Shapes rectangle1 = new Rectangle(5, 7);
-        Shapes rectangle2 = new Rectangle(5, 7);
+        Shape rectangle1 = new Rectangle(5, 7);
+        Shape rectangle2 = new Rectangle(5, 7);
 
         System.out.println(rectangle1.equals(rectangle2));
     }
