d5b2c425e04ce38f0286b8d4fd76b735de7fbf13
GontaTV <gontatv@ipgg.sbras.ru>
2019-11-05 10:42:28 +0700

    Merge branch 'master' of C:\Users\gontatv\IdeaProjects\ITSchool_(OOP) with conflicts.

--- Shapes/src/ru/academits/ShapesMain.java
+++ Shapes/src/ru/academits/ShapesMain.java
@@ -1,18 +1,26 @@
 package ru.academits;
 
-import ru.academits.inteface.gonta.Shape;
+import ru.academits.inteface.gonta.Shapes;
 import ru.academits.shapes.gonta.Circle;
 import ru.academits.shapes.gonta.Rectangle;
 import ru.academits.shapes.gonta.Square;
 import ru.academits.shapes.gonta.Triangle;
-import ru.academits.sort_shapes.gonta.SortShapesArea;
-import ru.academits.sort_shapes.gonta.SortShapesPerimeter;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class ShapesMain {
+    static class SortShapesArea {
+        static Comparator<Shapes> AreaComparator = new Comparator<Shapes>() {
+            @Override
+            public int compare(Shapes s1, Shapes s2) {
+                return (int) (s2.getArea() - s1.getArea());
+            }
+        };
+    }
+
     public static void main(String[] args) {
-        Shape square = new Square(1);
+        Shapes square = new Square(1);
 
         System.out.println("----------КВАДРАТ----------");
         System.out.println("ширина квадрата = " + square.getWidth());
@@ -20,7 +28,7 @@ public class ShapesMain {
         System.out.println("площадь квадрата = " + square.getArea());
         System.out.println("периметр квадрата = " + square.getPerimeter());
 
-        Shape circle = new Circle(5);
+        Shapes circle = new Circle(5);
 
         System.out.println();
         System.out.println("----------ОКРУЖНОСТЬ----------");
@@ -29,16 +37,18 @@ public class ShapesMain {
         System.out.println("площадь окружности = " + circle.getArea());
         System.out.println("длина окружности = " + circle.getPerimeter());
 
-        Shape triangle = new Triangle(5, 3, 5, 1, 6, 9);
+        Shapes triangle = new Triangle(5, 3, 5, 1, 6, 9);
+        Shapes triangle1 = new Triangle(3, 3, 1, 8, 2, 9);
 
         System.out.println();
         System.out.println("----------ТРЕУГОЛЬНИК----------");
         System.out.println("ширина треугольника = " + triangle.getWidth());
         System.out.println("высота треугольника = " + triangle.getHeight());
         System.out.println("площадь треугольника = " + triangle.getArea());
+        System.out.println("площадь треугольника = " + triangle1.getArea());
         System.out.println("периметр треугольника = " + triangle.getPerimeter());
 
-        Shape rectangle = new Rectangle(4, 8);
+        Shapes rectangle = new Rectangle(4, 8);
 
         System.out.println();
         System.out.println("----------ПРЯМОУГОЛЬНИК----------");
@@ -47,7 +57,7 @@ public class ShapesMain {
         System.out.println("площадь прямоугольника = " + rectangle.getArea());
         System.out.println("периметр прямоугольника = " + rectangle.getPerimeter());
 
-        Shape[] shapes = {
+        Shapes[] shapes = {
                 new Rectangle(3, 5),
                 new Triangle(2, 2, 5, 4, 5, 6),
                 new Circle(3),
@@ -57,23 +67,22 @@ public class ShapesMain {
                 new Square(7),
                 new Circle(5)};
 
-        Arrays.sort(shapes, new SortShapesArea());
+        Arrays.sort(shapes, SortShapesArea.AreaComparator);
 
         System.out.println();
         System.out.println("----------ПЛОЩАДЬ ФИГУР----------");
 
-        for (Shape e : shapes) {
+        for (Shapes e : shapes) {
             System.out.println(e.getArea());
         }
 
         System.out.println();
         System.out.println("Фигура с максимальной площадью - " + shapes[0]);
         System.out.println();
-        Arrays.sort(shapes, new SortShapesPerimeter());
-        System.out.println("Фигура со вторым по величине периметром - " + shapes[1]);
+        System.out.println("Фигура со второй по величине площадью - " + shapes[1]);
 
-        Shape rectangle1 = new Rectangle(5, 7);
-        Shape rectangle2 = new Rectangle(5, 7);
+        Shapes rectangle1 = new Rectangle(5, 7);
+        Shapes rectangle2 = new Rectangle(5, 7);
 
         System.out.println(rectangle1.equals(rectangle2));
     }
--- Shapes/src/ru/academits/shapes/gonta/Triangle.java
+++ Shapes/src/ru/academits/shapes/gonta/Triangle.java
@@ -1,21 +1,23 @@
 package ru.academits.shapes.gonta;
 
-import ru.academits.inteface.gonta.Shape;
+import ru.academits.inteface.gonta.Shapes;
 
-public class Triangle implements Shape {
+import java.util.Objects;
+
+public class Triangle implements Shapes {
     private double x1;
-    private double y1;
     private double x2;
-    private double y2;
     private double x3;
+    private double y1;
+    private double y2;
     private double y3;
 
     public Triangle(double x1, double y1, double x2, double y2, double x3, double y3) {
         this.x1 = x1;
-        this.y1 = y1;
         this.x2 = x2;
-        this.y2 = y2;
         this.x3 = x3;
+        this.y1 = y1;
+        this.y2 = y2;
         this.y3 = y3;
     }
 
@@ -23,20 +25,20 @@ public class Triangle implements Shape {
         return x1;
     }
 
-    public double getY1() {
-        return y1;
-    }
-
     public double getX2() {
         return x2;
     }
 
-    public double getY2() {
-        return y2;
-    }
-
     public double getX3() {
         return x3;
+    }
+
+    public double getY1() {
+        return y1;
+    }
+
+    public double getY2() {
+        return y2;
     }
 
     public double getY3() {
@@ -47,20 +49,20 @@ public class Triangle implements Shape {
         this.x1 = x1;
     }
 
-    public void setY1(double y1) {
-        this.y1 = y1;
-    }
-
     public void setX2(double x2) {
         this.x2 = x2;
     }
 
-    public void setY2(double y2) {
-        this.y2 = y2;
-    }
-
     public void setX3(double x3) {
         this.x3 = x3;
+    }
+
+    public void setY1(double y1) {
+        this.y1 = y1;
+    }
+
+    public void setY2(double y2) {
+        this.y2 = y2;
     }
 
     public void setY3(double y3) {
@@ -78,71 +80,46 @@ public class Triangle implements Shape {
         }
 
         Triangle triangle = (Triangle) o;
-        return triangle.x1 == x1 &&
-                triangle.y1 == y1 &&
-                triangle.x2 == x2 &&
-                triangle.y2 == y2 &&
-                triangle.x3 == x3 &&
-                triangle.y3 == y3;
+        return Double.compare(triangle.x1, x1) == 0 &&
+                Double.compare(triangle.x2, x2) == 0 &&
+                Double.compare(triangle.x3, x3) == 0 &&
+                Double.compare(triangle.y1, y1) == 0 &&
+                Double.compare(triangle.y2, y2) == 0 &&
+                Double.compare(triangle.y3, y3) == 0;
     }
 
     @Override
     public int hashCode() {
-        final int prime = 37;
-        int hash = 1;
-        hash = prime * hash + Double.hashCode(x1);
-        hash = prime * hash + Double.hashCode(y1);
-        hash = prime * hash + Double.hashCode(x2);
-        hash = prime * hash + Double.hashCode(y2);
-        hash = prime * hash + Double.hashCode(x3);
-        hash = prime * hash + Double.hashCode(y3);
-        return hash;
+        return Objects.hash(x1, x2, x3, y1, y2, y3);
     }
 
     @Override
     public String toString() {
-        return "Треугольник:" + System.lineSeparator() +
-                "координаты (х1 = " + x1 + "; y1 = " + y1 + "; x2 = " + x2 + "; y2 = " + y2 + "; x3 = " + x3 + "; y3 = " + y3 + ")" +
-                System.lineSeparator() +
-                "высота = " + getHeight() + System.lineSeparator() +
-                "ширина = " + getWidth() + System.lineSeparator() +
-                "площадь = " + getArea() + System.lineSeparator() +
-                "периметр = " + getPerimeter();
+        return "Треугольник: \nвысота = " + getHeight() + "\nширина = " + getWidth() + "\nплощадь = " + getArea() + "\nпериметр = " + getPerimeter();
     }
 
-    private double[] getLineLength() {
-        double[][] array = {{x1, x2, x3}, {y1, y2, y3}};
-        double[] lineLength = new double[3];
-
-        for (int i = 0, j = 1; i <= array.length; i++, j++) {
-            if (j > array.length) {
-                j = 0;
-            }
-            lineLength[i] = Math.sqrt(Math.pow((array[0][i] - array[0][j]), 2) + Math.pow((array[1][i] - array[1][j]), 2));
-        }
-        return lineLength;
+    private static double lineABLength(double x1, double x2, double y1, double y2) {
+        return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
     }
 
     @Override
     public double getWidth() {
-        return Math.max(Math.max(x1, x2), x3) - Math.min(Math.min(x1, x2), x3);
+        return ((x1 > x2) ? (Math.max(x1, x3)) : (Math.max(x2, x3))) - ((x1 < x2) ? (Math.min(x1, x3)) : (Math.min(x2, x3)));
     }
 
     @Override
     public double getHeight() {
-        return Math.max(Math.max(y1, y2), y3) - Math.min(Math.min(y1, y2), y3);
+        return ((x1 > x2) ? (Math.max(y1, y3)) : (Math.max(y2, y3))) - ((y1 < y2) ? (Math.min(y1, y3)) : (Math.min(y2, y3)));
     }
 
     @Override
     public double getPerimeter() {
-        double[] lineLength = getLineLength();
-        return lineLength[0] + lineLength[1] + lineLength[2];
+        return lineABLength(x1, x2, y1, y2) + lineABLength(x2, x3, y2, y3) + lineABLength(x1, x3, y1, y3);
     }
 
     @Override
     public double getArea() {
-        double[] lineLength = getLineLength();
-        double perimeterHalf = getPerimeter() / 2;
-        return Math.sqrt(perimeterHalf * (perimeterHalf - lineLength[0]) * (perimeterHalf - lineLength[1]) * (perimeterHalf - lineLength[2]));
+
+        return Math.sqrt((getPerimeter() / 2) * ((getPerimeter() / 2) - lineABLength(x1, x2, y1, y2)) * ((getPerimeter() / 2) - lineABLength(x2, x3, y2, y3)) * ((getPerimeter() / 2) - lineABLength(x1, x3, y1, y3)));
     }
 }
