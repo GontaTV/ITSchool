bce39f5676cb910f2b477f249ae47d637ab6010f
GontaTV <gontatv@ipgg.sbras.ru>
2019-11-18 23:42:24 +0700

    исправлены замечания

--- ArrayListHome/src/ArrayListHome.java
+++ ArrayListHome/src/ArrayListHome.java
@@ -4,36 +4,52 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ArrayListHome {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(new FileInputStream(".idea/file"))) {
+            while (scanner.hasNextLine()) {
+                strings.add(scanner.nextLine());
+            }
+            System.out.println("Список строк " + strings);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Ошибка! Файл не найден");
+        }
+
         ArrayList<Integer> numbers = new ArrayList<>();
 
-        Scanner scanner = new Scanner(new FileInputStream(".idea/file"));
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(6);
+        numbers.add(5);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
 
-        while (scanner.hasNext()) {
-            numbers.add(scanner.nextInt());
-        }
+        System.out.println("Исходный список " + numbers);
 
-        System.out.print("Исходный список " + numbers);
-
-        for (int i = 0; i < numbers.size(); i++) {
-            if (numbers.get(i) % 2 == 0) {
-                numbers.remove(i);
-                i--;
+        try {
+            for (int i = 0; i < numbers.size(); i++) {
+                if (numbers.get(i) % 2 == 0) {
+                    numbers.remove(i);
+                    i--;
+                }
             }
-        }
+            System.out.println("Список, после удаления четных чисел " + numbers);
 
-        System.out.println();
-        System.out.print("Список, после удаления четных чисел " + numbers);
+            ArrayList<Integer> uniqueNumbers = new ArrayList<>();
 
-        ArrayList<Integer> uniqueNumbers = new ArrayList<>();
-
-        for (Integer n : numbers) {
-            if (uniqueNumbers.indexOf(n) == -1) {
-                uniqueNumbers.add(n);
+            for (Integer n : numbers) {
+                if (!uniqueNumbers.contains(n)) {
+                    uniqueNumbers.add(n);
+                }
             }
-        }
+            System.out.print("Список, содержащий уникальные числа " + uniqueNumbers);
 
-        System.out.println();
-        System.out.print("Список, содержащий уникальные числа " + uniqueNumbers);
+        } catch (NullPointerException e) {
+            System.out.println("Ошибка! Значения списка не должны = null");
+        }
     }
 }
